47c4341cb2b3f89ed1c98b7e0dddaf300d5bcafc
happygao <1254702914@qq.com>
2020-11-06 09:34:15 +0800

    修改了main.js

--- src/main.js
+++ src/main.js
@@ -10,13 +10,13 @@ import router from './plugins/router.js' //引入路由
 
 import './plugins/axios'; //拦截器
 
-import './filters/index.js'  //全局过滤器
-
-
+import './filters/index.js'  //全局过滤
+var a = 35346;
+console.log(a)
 // import './plugins/vant.js'
 //引入第三方组件库 vant
 import 'vant/lib/index.css'
-import { Tab, Tabs,NavBar} from 'vant';
+import { Tab, Tabs, NavBar } from 'vant';
 Vue.use(Tab);
 Vue.use(Tabs);
 Vue.use(NavBar);
@@ -25,10 +25,10 @@ Vue.use(NavBar);
 let vm = new Vue({
   render: h => h(App),
   router,
-  data:{
-	  bLoading:false,
-	  bFoot:true
+  data: {
+    bLoading: false,
+    bFoot: true
   }
 }).$mount('#app')
 
- export default vm;
+export default vm;
